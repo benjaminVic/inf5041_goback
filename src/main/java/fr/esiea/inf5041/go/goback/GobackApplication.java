af3c528cd359b8a7a63347619afe59805e3a14fe
benjaminVic <benjamin.vic.lallart@gmail.com>
2018-09-17 13:34:48 +0200

    Initial Commit

---
+++ src/main/java/fr/esiea/inf5041/go/goback/GobackApplication.java
@@ -0,0 +1,12 @@
+package fr.esiea.inf5041.go.goback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GobackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GobackApplication.class, args);
+    }
+}
---
+++ src/test/java/fr/esiea/inf5041/go/goback/GobackApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.esiea.inf5041.go.goback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GobackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
